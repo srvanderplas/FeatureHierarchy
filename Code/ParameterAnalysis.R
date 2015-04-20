c806fe5329d3ddb279e1d8aaf710e7b9b02cb8f3
Susan VanderPlas <srvanderplas@gmail.com>
2015-04-20 17:19:16 -0500

    Comments and simulation data

--- Code/ParameterAnalysis.R
+++ Code/ParameterAnalysis.R
@@ -196,33 +196,63 @@ library(plyr)
 library(reshape2)
 library(ggplot2)
 library(grid)
+# 
+# # Simulation parameters
+# data.parms <- expand.grid(sd.trend=round(c(.25, .35, .45), 2),
+#                                sd.cluster=1:3,
+#                                K=c(3, 5))
+# 
+# data.parms$sd.cluster[data.parms$K==5] <- c(.2, .25, .3)[data.parms$sd.cluster[data.parms$K==5]]
+# data.parms$sd.cluster[data.parms$K==3] <- c(.25, .3, .35)[data.parms$sd.cluster[data.parms$K==3]]
+# data.parms$sd.cluster <- round(data.parms$sd.cluster, 2)
+# data.parms$N <- data.parms$K*15
+# 
+# 
+# tmp <- function(M=1000, N=45, K=3, sT=0.3, sC=0.3) {
+#   do.call("rbind", lapply(1:M, function(i) {
+#     input.pars <- list(N=N, K=K, sd.trend=sT, sd.cluster=sC)
+#     dat <- gen.data(input.pars)
+#     res <- ddply(dat, .(.sample), eval.df)
+#     res$trend.target <- unique(dat$target1)
+#     res$cluster.target <- unique(dat$target2)
+#     res$rep <- i
+#     data.frame(res, input.pars)
+#   }))
+# }
+# nulldist<- cmpfun(tmp)
+# 
+# simulation.results <- ldply(1:nrow(data.parms), function(i) with(data.parms[i,], nulldist(M=500, N=N, K=K, sT=sd.trend, sC=sd.cluster)), .parallel=T)
+# names(simulation.results) <- c("sample", "R2", "C2", "Gini", "trend.target", "cluster.target", "rep", "N", "K", "sd.trend", "sd.cluster")
+# simulation.results$sd.trend <- round(simulation.results$sd.trend, 2)
+# simulation.results$sd.cluster <- round(simulation.results$sd.cluster, 2)
+# save(simulation.results, file = "./Data/Turk16ParameterSimulation.Rdata")
 
-# Simulation parameters
-data.parms <- expand.grid(sd.trend=round(c(.25, .35, .45), 2),
-                               sd.cluster=1:3,
-                               K=c(3, 5))
+load("./Data/Turk16ParameterSimulation.Rdata")
 
-data.parms$sd.cluster[data.parms$K==5] <- c(.2, .25, .3)[data.parms$sd.cluster[data.parms$K==5]]
-data.parms$sd.cluster[data.parms$K==3] <- c(.25, .3, .35)[data.parms$sd.cluster[data.parms$K==3]]
-data.parms$sd.cluster <- round(data.parms$sd.cluster, 2)
-data.parms$N <- data.parms$K*15
+simulation.results$sdLine <- paste("sigma[T] :",simulation.results$sd.trend)
+simulation.results$sdGroup <- paste("sigma[C] :", simulation.results$sd.cluster)
+simulation.results$target <- paste0(c("", "trend")[(simulation.results$sample==simulation.results$trend.target)+1], c("", "cluster")[(simulation.results$sample==simulation.results$cluster.target) + 1])
+simulation.results$target[simulation.results$target==""] <- "decoy"
+
+ggplot(data=simulation.results) + 
+  geom_point(aes(x=R2, y=rep, color=target, size=target), shape=1, alpha=.5) + 
+  facet_grid(sdLine~K+sdGroup, labeller=label_parsed) +
+  scale_size_manual(values=c(1.5, 1, 1.5)) + 
+  scale_colour_manual(values=c("red", "grey50", "blue")) +
+  theme_bw() + 
+  ggtitle("Change in R^2 with parameter values")
+
+ggplot(data=simulation.results) + 
+  geom_point(aes(x=C2, y=rep, color=target, size=target), shape=1, alpha=.5) + 
+  facet_grid(sdGroup~K+sdLine, labeller=label_parsed) +
+  scale_size_manual(values=c(1.5, 1, 1.5)) + 
+  scale_colour_manual(values=c("red", "grey50", "blue")) +
+  theme_bw() + 
+  ggtitle("Change in C^2 with parameter values")
 
 
-tmp <- function(M=1000, N=45, K=3, sT=0.3, sC=0.3) {
-  do.call("rbind", lapply(1:M, function(i) {
-    input.pars <- list(N=N, K=K, sd.trend=sT, sd.cluster=sC)
-    dat <- gen.data(input.pars)
-    res <- ddply(dat, .(.sample), eval.df)
-    res$trend.target <- unique(dat$target1)
-    res$cluster.target <- unique(dat$target2)
-    res$rep <- i
-    data.frame(res, input.pars)
-  }))
-}
-nulldist<- cmpfun(tmp)
+# I think that we have a case of some fairly bad random samples... combined with the fact that values were REALLY compressed for K=5, particularly in the sigma_T=0.25, sigma_C=0.3 condiditon, where blue dots are actually higher than red dots for C^2. (The analogous condition for K=3 shows some signs of this starting to occur, though...)
 
-simulation.results <- ldply(1:nrow(data.parms), function(i) with(data.parms[i,], nulldist(M=500, N=N, K=K, sT=sd.trend, sC=sd.cluster)), .parallel=T)
-names(simulation.results) <- c("sample", "R2", "C2", "Gini", "trend.target", "cluster.target", "rep", "N", "K", "sd.trend", "sd.cluster")
-simulation.results$sd.trend <- round(simulation.results$sd.trend, 2)
-simulation.results$sd.cluster <- round(simulation.results$sd.cluster, 2)
-save(simulation.results, file = "./Data/Turk16ParameterSimulation.Rdata")
+# The bigger problem for us is that for R^2, red and blue were on opposite sides of the distribution - this isn't true for C^2, which could mean that we made clustering a bit harder by default. Of course, our results show that if that's the case, we're really good at clustering.
+# It could also be that we're balancing out - "this plot has no linear trend" vs. "strong linear trend", compared with "strong clustering in a trend plot" vs. "strong clustering in a cluster plot". 
+
